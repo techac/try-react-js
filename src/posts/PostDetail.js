8feb38b3fc940a7d29f2054334e45b27f74daa33
techac <achugh519@gmail.com>
2018-10-13 22:37:51 +0530

    Handle clickk events suing on click

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 class PostDetail extends Component {
+  titleWasClicked(event){
+    alert("Hello There");
+  }
   render () {
     const {post} = this.props
     return (
       <div>
-        <h1>{post.title}</h1>
+        <h1 onClick={this.titleWasClicked}>{post.title}</h1>
         <p>{post.content}</p>
       </div>
     )
